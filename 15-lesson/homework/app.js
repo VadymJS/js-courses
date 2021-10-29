9d977c93e71fb4f9c57daaf5255a4f2cbf6fb929
VadymJS <vadim.travel12@gmail.com>
2021-10-29 14:33:54 +0300

    Add homework 15

---
+++ 15-lesson/homework/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const inputEl = document.querySelector('#input');
+const btnEl = document.querySelector('#btn');
+const containerEl = document.querySelector('.user-info');
+btnEl.addEventListener('click', () => userCheck(inputEl.value));
+
+function userCheck(login) {
+    if (inputEl.value) {
+        axios.get(`https://api.github.com/users/${login}`)
+            .then(function (response) {
+            containerEl.innerHTML = 
+                '<img src="' + response.data.avatar_url + '">' + 
+                '<p>Public repos:' + ' ' + response.data.public_repos + 
+                '<p>' +'<p>Followers:' + ' ' + response.data.followers + 
+                '<p>' + '<p>Following:' + ' ' + response.data.following + '<p>'
+            ;
+        })
+            .catch(function (error) {
+            containerEl.innerHTML = '<p>Error, try again';
+        });
+    }
+    
+}
