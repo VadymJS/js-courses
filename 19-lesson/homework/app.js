da5f922ad933486c127357dbad3284be8449ce0c
VadymJS <vadim.travel12@gmail.com>
2021-11-24 13:57:10 +0200

    Add homework 19

---
+++ 19-lesson/homework/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const calc = require('./operations');
+
+console.log('Add: ', calc.add(5,7));
+console.log('Minus: ', calc.minus(10,5));
+console.log('Multiply: ', calc.multiply(5,5));
+console.log('Divide: ', calc.divide(20,5));
---
+++ 19-lesson/homework/operations/index.js
@@ -0,0 +1,6 @@
+const add = require('./add');
+const minus = require('./minus');
+const multiply = require('./multiply');
+const divide = require('./divide');
+
+module.exports = { add, minus, multiply, divide };
